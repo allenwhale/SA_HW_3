964af416cfed822631c90809c62ee57232d400de
allenwhale <allenwhale@yahoo.com.tw>
2014-10-31 20:50:58 +0800

    add rss/rss.sh rss/myfeed.py

---
+++ rss/myfeed.py
@@ -0,0 +1,139 @@
+#!/usr/bin/env python3
+import subprocess
+import lxml.html
+import argparse
+RSS_DIR = '/home/allenwhale/.feed'
+TITLE_FILE = RSS_DIR+'/title'
+TEMP_FILE = RSS_DIR+'/.tmp'
+class RSS:
+    def __init__(self, url):
+        assert(url != "")
+        self.url = url
+
+        try:
+            main = lxml.html.parse(self.url)
+            if main.xpath("/html/body/rss"):
+                self.title = main.xpath("/html/body/rss/channel/title")[0].text
+                self.desc = main.xpath("/html/body/rss/channel/description")[0].text
+                self.items = list(zip(
+                    [ x.text.replace('\"',' ').replace('\'',' ') for x in main.xpath("/html/body/rss/channel/item[*]/title") ],
+                    [ x.text.replace('\"',' ').replace('\'',' ')for x in main.xpath('/html/body/rss/channel/item[*]/guid') ],
+                    [ x.text.split('<')[0].replace('\"',' ').replace('\'',' ') for x in main.xpath("/html/body/rss/channel/item[*]/description") ]))
+            elif main.xpath("/html/body/feed"):
+                self.title = main.xpath("/html/body/feed/title")[0].text
+                self.desc = main.xpath("/html/body/feed/subtitle")[0].text
+                self.items = list(zip(
+                    [ x.text.replace('\"',' ').replace('\'',' ') for x in main.xpath("/html/body/feed/entry[*]/title") ],
+                    [ x.text.replace('\"',' ').replace('\'',' ') for x in main.xpath('/html/body/feed/entry[*]/origlink') ],
+                    [ x.text.split('<')[0].replace('\"',' ').replace('\'',' ') for x in main.xpath("/html/body/feed/entry[*]/content") ]))
+            else:
+                pass
+
+        except:
+            f = open(TEMP_FILE,'w')
+            f.write("wrong")
+            f.close()
+            raise RuntimeError
+
+    def get_url(self):
+        return self.url
+
+    def get_title(self):
+        return self.title.split(' ')[0]
+
+    def get_desc(self):
+        return self.desc
+
+    def get_items(self):
+        return self.items
+
+    def get_nth_item(self, n):
+        assert( n < len(self.items))
+        return self.items[n]
+
+
+
+def __test():
+    r = RSS('http://feeds.bbci.co.uk/news/rss.xml')
+    r = RSS('http://rss.slashdot.org/Slashdot/slashdot')
+    r = RSS('http://www.phoronix.com/rss.php')
+    r = RSS('http://www.36kr.com/feed/')
+def write_title(title_list):
+    f = open(TITLE_FILE,"w")
+    for i in title_list:
+        s = i[0]+"\n"+i[1]+'\n'
+        f.write(s)
+    f.close()
+    return None
+def write_items(title,items):
+    f = open(RSS_DIR+'/'+title,'w')
+    for i in items:
+        s = i[0]+'\n'+i[1]+'\n'+i[2]+'\n'
+        f.write(s)
+    f.close();
+    return None
+def get_title_list():
+    f = open(TITLE_FILE,"r")
+    title_list = []
+    tmp = None
+    for i in f:
+        if tmp==None:
+            tmp = (i.replace('\n',''),None)
+        else:
+            tmp = (tmp[0],i.replace('\n',''))
+            title_list.append(tmp)
+            tmp = None
+    return title_list
+def add_feed(url):
+    rss = RSS(url)
+    title = rss.get_title()
+    items = rss.get_items()
+    title_list = get_title_list()
+    for i in title_list:
+        if i[0]==title:
+            return 'Exist'
+    title_list.append((title,url))
+    write_title(title_list)
+    write_items(title,items)
+    return None
+def update_feed(url, title):
+    title_list = get_title_list()
+    if (title,url) not in title_list:
+        return 'NExist'
+    rss = RSS(url)
+    items = rss.get_items()
+    write_items(title,items)
+    return None
+def delete_feed(title):
+    title_list = get_title_list()
+    new_title_list = []
+    for i in title_list:
+        if i[0]!=title:
+            new_title_list.append(i)
+    write_title(new_title_list)
+    subprocess.call(['rm','-rf',RSS_DIR+'/'+title])
+    return None
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='RSS fetcher')
+    parser.add_argument('--add', '-a' ,  type=str,  help='add feed  url')
+    parser.add_argument('--update', '-u', nargs=2, help='update "url" "title"' )
+    parser.add_argument('--delete', '-d', type=str, help='delete the feed "title"' )
+    args = parser.parse_args()
+    f = open(TEMP_FILE,'w')
+    if args.add:
+        res = add_feed(args.add)
+        if res:
+            f.write(res)
+    elif args.update:
+        res = update_feed(args.update[0], args.update[1])
+        if res:
+            f.write(res)
+    elif args.delete:
+        res = delete_feed(args.delete)
+        if res:
+            f.write(res)
+    else:
+        pass
+    f.close()
+
---
+++ rss/rss.sh
@@ -0,0 +1,261 @@
+#!/bin/sh
+RSS_DIR='/home/allenwhale/.feed'
+TEMP_FILE=${RSS_DIR}'/.tmp'
+TITLE_FILE=${RSS_DIR}'/title'
+ITEM_FILE=${RSS_DIR}'/item_view'
+SUSCRIPTION=''
+NOW_PAGE='Welcome'
+Welcome(){
+    env dialog --title 'RSS Reader' --msgbox 'My RSS Reader' 0 0 
+    NOW_PAGE='Menu'
+}
+Menu(){
+    env dialog --menu 'Choose Action' 0 0 5 'R' 'Read - read subscribed feeds' 'S' 'Subscribe - new subscription' 'D' 'Delete - delete subscription' 'U' 'Update - update subscription' 'Q' 'Bye' 2>${TEMP_FILE}
+    action=$(cat ${TEMP_FILE})
+    env rm ${TEMP_FILE}
+    case ${action} in
+	'R')
+	    NOW_PAGE='Read'
+	    ;;
+	'S')
+	    NOW_PAGE='Subscribe'
+	    ;;
+	'D')
+	    NOW_PAGE='Delete'
+	    ;;
+	'U')
+	    NOW_PAGE='Update'
+	    ;;
+	'Q')
+	    NOW_PAGE='Quit'
+	    ;;
+    esac
+
+}
+Read(){
+    s=''
+    flag='1'
+    cnt='1'
+    while read line ; do
+	if [ ${#line} -eq 0 ] ; then
+	    return
+	fi
+	if [ $flag -eq 1 ] ; then
+	    s=${s}' '${cnt}' '${line}' '
+	    flag='0'
+	    eval TITLE_NAME$cnt='${line}'
+	    cnt=$((${cnt} + 1))
+	else
+	    flag='1'
+	fi
+    done < ${TITLE_FILE}
+    if [ ${#s} -eq 0 ] ; then 
+	env dialog --title 'Read' --msgbox 'No Subscription' 0 0 
+	NOW_PAGE='Menu'
+	return 
+    fi
+    env dialog --title 'Read' --menu 'choose subscription' 0 0 $(($cnt - 1)) ${s} 2>${TEMP_FILE}
+    if [ $? -eq 1 ] ; then 
+	NOW_PAGE='Menu'
+	return
+    fi
+    SUBSCRIPTION=$(cat ${TEMP_FILE})
+    env rm ${TEMP_FILE}
+    eval SUBSCRIPTION=\$TITLE_NAME$SUBSCRIPTION
+    NOW_PAGE='ReadItems'
+}
+ReadItems(){
+    flag=1
+    cnt=1
+    s=''
+    while read line ; do
+	if [ ${flag} -eq 1 ] ; then
+	    eval SUBTITLE${cnt}='${line}'
+	    s=${s}' '${cnt}' '\""${line}"\"' '
+	    flag=2
+	elif [ ${flag} -eq 2 ] ; then
+	    eval URL${cnt}='${line}'
+	    flag=3
+	else
+	    eval CONTENT${cnt}=\""${line}"\"
+	    cnt=$((${cnt} + 1))
+	    flag=1
+	fi
+    done < ${RSS_DIR}'/'${SUBSCRIPTION}
+    eval env dialog --title \'Read\' --menu \'choose item\' 0 0 $((${cnt} - 1)) ${s} 2>${TEMP_FILE}
+    if [ $? -eq 1 ] ; then
+	NOW_PAGE='Read'
+	return
+    fi
+    itemno=$(env cat ${TEMP_FILE})
+    env rm ${TEMP_FILE}
+    eval IN_TITLE=\$SUBTITLE${itemno}
+    eval IN_URL=\$URL${itemno}
+    eval IN_CONTENT=\$CONTENT${itemno}
+    NOW_PAGE='Items'
+}
+Items(){
+    echo "${IN_TITLE}" > ${ITEM_FILE}
+    echo '==============================================' >> ${ITEM_FILE}
+    echo "${IN_URL}" >> ${ITEM_FILE}
+    echo '==============================================' >> ${ITEM_FILE}
+    cnt=0
+    for i in ${IN_CONTENT} ; do
+	echo -n "${i} " >> ${ITEM_FILE}
+	cnt=$((${cnt} + ${#i}))
+	if [ ${cnt} -gt 50 ] ; then
+	    echo '' >>${ITEM_FILE}
+	    cnt=0
+	fi
+    done
+    if [ ${cnt} -ne 0 ] ; then 
+	echo '' >> ${ITEM_FILE}
+    fi
+    echo '==============================================' >> ${ITEM_FILE}
+    env dialog --textbox ${ITEM_FILE} 0 0
+    NOW_PAGE='ReadItems'
+}
+Subscribe(){
+    env dialog  --inputbox 'Enter feed url' 0 0 2>${TEMP_FILE}
+    if [ $? -eq 1 ] ; then
+	NOW_PAGE='Menu'
+	return
+    fi
+    url=$(cat ${TEMP_FILE})
+    env rm ${TEMP_FILE}
+    if [ ${#url} -eq 0 ] ; then
+	env dialog --title 'Subscribe' --msgbox 'url can not be empty' 0 0
+	return
+    fi
+    env python3 myfeed.py -a $url
+    res=$(env cat ${TEMP_FILE})
+    env rm ${TEMP_FILE}
+    if [ ${#res} -eq 0 ] ; then
+	env dialog --title 'Subscribe' --msgbox 'Success' 0 0
+	NOW_PAGE='Menu'
+    else
+	env dialog --title 'Subscribe' --msgbox 'Something Wrong' 0 0
+    fi
+}
+Delete(){
+    s=''
+    flag='1'
+    cnt='1'
+    while read line ; do
+	if [ $flag -eq 1 ] ; then
+	    s=${s}' '${cnt}' '${line}' '
+	    flag='0'
+	    eval TITLE_NAME$cnt='${line}'
+	    cnt=$((${cnt} + 1))
+	else
+	    flag='1'
+	fi
+    done < ${TITLE_FILE}
+    if [ ${#s} -eq 0 ] ; then 
+	env dialog --title 'Delete' --msgbox 'No Subscription' 0 0
+	NOW_PAGE='Menu'
+	return 
+    fi
+    env dialog --title 'Delete' --menu 'choose item to delete' 0 0 ${cnt} ${s} 2>${TEMP_FILE}
+    if [ $? -eq 1 ] ; then 
+	NOW_PAGE='Menu'
+	return
+    fi
+    SUBSCRIPTION=$(cat ${TEMP_FILE})
+    env rm ${TEMP_FILE}
+    eval SUBSCRIPTION=\$TITLE_NAME$SUBSCRIPTION
+    env python3 myfeed.py -d $SUBSCRIPTION
+    tmp=$(cat ${TEMP_FILE})
+    if [ ${#tmp} -eq 0 ] ; then
+	env dialog --title 'Delete' --msgbox 'OK' 0 0
+    else
+	env dialog --title 'Delete' --msgbox 'Something Wrong' 0 0
+    fi
+}
+Update(){
+    s=''
+    flag='1'
+    cnt='1'
+    while read line ; do
+	if [ $flag -eq 1 ] ; then
+	    s=${s}' '${cnt}' '${line}' off '	    
+	    flag='0'
+	    eval TITLE_NAME$cnt='${line}'
+	else
+	    eval TITLE_URL$cnt='${line}'
+	    cnt=$((${cnt} + 1))
+	    flag='1'
+	fi
+    done < ${TITLE_FILE}
+    if [ ${#s} -eq 0 ] ; then 
+	env dialog --title 'Update' --msgbox 'No Subscription' 0 0
+	NOW_PAGE='Menu'
+	return 
+    fi
+    env dialog --title 'Update' --checklist 'choose item to delete' 0 0 ${cnt} ${s} 2>${TEMP_FILE}
+    if [ $? -eq 1 ] ; then 
+	NOW_PAGE='Menu'
+	return
+    fi
+    SUBSCRIPTION=$(cat ${TEMP_FILE})
+    env rm ${TEMP_FILE}
+    cnt=0
+    for i in $SUBSCRIPTION ; do
+	cnt=$((${cnt} + 1))
+    done
+    OK=0
+    no=1
+    for i in $SUBSCRIPTION ; do
+        eval SUBSCRIPTION=\$TITLE_NAME$i
+	eval SUBSCRIPTIONURL=\$TITLE_URL$i
+	env python3 myfeed.py -u $SUBSCRIPTIONURL $SUBSCRIPTION 
+	tmp=$(cat ${TEMP_FILE})
+	env rm ${TEMP_FILE}
+	if [ ${#tmp} -eq 0 ] ; then
+	    echo $((100 * ${no} / ${cnt}))
+	    no=$((${no} + 1))
+	else
+	    OK=1
+	    break
+	fi
+    done | env dialog --title 'Update' --guage 'Please wait' 0 0
+    if [ ${OK} -eq 0 ] ; then 
+	env dialog --title 'Update' --msgbox 'Success' 0 0
+    else
+	env dialog --title 'Update' --msgbox 'Something Wrong' 0 0
+    fi
+    NOW_PAGE='Menu'
+}
+Quit(){
+    env dialog --title 'Quit?' --yesno 'Do you want to quit?' 0 0
+    tmp=$?
+    if [ $tmp -eq 1 ] ; then
+	NOW_PAGE='Menu'
+    else
+	NOW_PAGE='Exit'
+    fi
+}
+while [ 1=1 ]; do
+    case ${NOW_PAGE} in 
+	'Welcome')
+	    Welcome;;
+	'Menu')
+	    Menu;;
+	'Read')
+	    Read;;
+	'ReadItems')
+	    ReadItems;;
+	'Items')
+	    Items;;
+	'Subscribe')
+	    Subscribe;;
+	'Update')
+	    Update;;
+	'Delete')
+	    Delete;;
+	'Quit')
+	    Quit;;
+	'Exit')
+	    break;;
+    esac
+done
