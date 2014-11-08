ed6ebff59260d4749cc1d34fc7c1d0556e43006e
allenwhale <allenwhale@yahoo.com.tw>
2014-11-09 02:02:35 +0800

    add poly2.cpp

---
+++ polyglot/poly2.cpp
@@ -0,0 +1,14 @@
+x={-
+#if 0
+0+"""".to_i => 0}
+print `cat flag`
+__END__
+""".find('x')};
+import os
+p=os.system;{
+#endif
+0};
+#include <stdlib.h>
+#define p(s) int main(){system(s);}
+p("cat flag");
+#define x // -} ();main = do x <- readFile "flag"; putStr x
